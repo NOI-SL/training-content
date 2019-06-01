fd79ce7a9144df0e948327a64702d6950fcb3765
Pasindu Wijesena <pasindu.wijesena@gmail.com>
2019-06-01 10:43:27 +0530

    recursion content

---
+++ recursion/fibonacii.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// n        0   1   2   3   4   5   6   ...
+// fib(n)   0   1   1   2   3   5   8   ...
+int fibonacii(int n){
+  if(n==0) return 0;
+  if(n==1) return 1;
+
+  return fibonacii(n-1) + fibonacii(n-2);
+}
+
+int main(){
+  int N;
+  while(true){
+    cin>>N;
+    cout<<fibonacii(N)<<endl;
+  }
+  return 0;
+}
