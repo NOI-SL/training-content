4bd0d94a51bf6f1d259d545a96c1427dc1d68f1f
Pasindu Wijesena <pasindu.wijesena@gmail.com>
2019-06-01 10:48:54 +0530

    bubble sort and inputs

---
+++ sorting/bubble-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void sort(int N, int array[]){
+  for(int i=0; i<N-1; i++){
+    for(int j=0; j<N-1; j++){
+      if(array[j] > array[j+1]){
+        int temp = array[j];
+
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+}
+
+int main(){
+  int N;
+  cin>>N;
+  int array[N];
+  for(int i=0; i<N; i++){
+    cin>>array[i];
+  }
+
+  cout<<"Going to sort the input array"<<endl;
+  sort(N, array);
+  cout<<"Sorted"<<endl;
+
+  for(int i=0; i<N; i++){
+    cout<<array[i]<<' ';
+  }
+  cout<<endl;
+  return 0;
+}
